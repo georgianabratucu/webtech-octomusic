c0a4f22c49fc5e1c9a0832d8776b196d17812872
Bratucu Iuliana <iuliana.bratucu.97@gmail.com>
2018-12-04 18:20:02 +0000

    accounts table

--- server.js
+++ server.js
@@ -13,4 +13,22 @@ sequelize.authenticate().then(function(){
     console.log("there was an error connecting to db");
 })
 
+//create tables
+const Accounts = sequelize.define('accounts', {
+    
+    name: Sequelize.STRING,
+    username: {type:Sequelize.STRING,
+               unique:{msg:'This username already exists'},
+               allowNull:false},
+    password: {type:Sequelize.STRING,
+                allowNull:false,
+                validate:{len:{args:[5,15], msg:'Password must have between 5 and 15 characters'}}},
+    email: { type:Sequelize.STRING,
+             validate: {isEmail:{msg: 'Email address is not valid'}}
+            },
+    birth_date:Sequelize.DATE
+})
+
+
+
 app.listen(8080);
