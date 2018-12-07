879ffb706d9ca541a04415b84af2c5f052fe875f
Bratucu Georgiana <bratucu_georgiana@yahoo.com>
2018-12-07 21:31:48 +0000

    functii asincrone

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express=require("express")
 const Sequelize=require("sequelize")
+const axios=require("axios")
+
 const app=express()
 app.use("/",express.static('static'))
 const sequelize = new Sequelize('Music', 'root', '', {
@@ -37,9 +39,9 @@ const Accounts = sequelize.define('accounts', {
                }
     },
     email: { 
-             type:Sequelize.STRING,
-               allowNull: false,
-             validate: {
+              type:Sequelize.STRING,
+              allowNull:false,
+              validate: {
                 isEmail:{
                     msg: 'Email address is not valid'}}
             },
@@ -48,7 +50,8 @@ const Accounts = sequelize.define('accounts', {
 
 const GeoTracks = sequelize.define('geo_tracks', {
     
-    name: {type:Sequelize.STRING,
+    name: {
+           type:Sequelize.STRING,
            allowNull: false,
     },
     listeners: Sequelize.INTEGER,
@@ -56,10 +59,7 @@ const GeoTracks = sequelize.define('geo_tracks', {
     image: Sequelize.STRING,
     rank: {
             type: Sequelize.INTEGER,
-            allowNull: false,
-            validate: {
-                isInt: {msg: 'Only numbers accepted'}
-                }
+            allowNull:false
             },
     country: Sequelize.STRING,
     id_artist:Sequelize.INTEGER
@@ -67,7 +67,8 @@ const GeoTracks = sequelize.define('geo_tracks', {
 
 const GenreTracks = sequelize.define('genre_tracks', {
     
-     name:{ type:Sequelize.STRING,
+     name:{ 
+             type:Sequelize.STRING,
              allowNull: false
           
         },
@@ -75,11 +76,8 @@ const GenreTracks = sequelize.define('genre_tracks', {
     url: Sequelize.STRING,
     image: Sequelize.STRING,
     rank: {type:Sequelize.INTEGER,
-           allowNull: false,
-           validate: {
-                isInt: true,
-                msg:'Only numbers accepted'
-            }
+           allowNull:false,
+         
     },
     rank: Sequelize.INTEGER,
     genre: Sequelize.STRING,
@@ -90,7 +88,7 @@ const Artists=sequelize.define('artists',{
     
     name: {
         type:Sequelize.STRING,
-        allowNull: false
+        allowNull:false
     },
     listeners:Sequelize.INTEGER,
     url:Sequelize.STRING,
@@ -103,10 +101,7 @@ const Preferences=sequelize.define('preferences',{
     },
     mark:{
            type:Sequelize.INTEGER,
-            validate: {
-                isInt: true,
-                msg:'Only numbers accepted'
-            }
+        
     },
     id_user:{
               type:Sequelize.INTEGER,
@@ -130,6 +125,70 @@ GeoTracks.belongsTo(Artists,{foreignKey:'id_artist'});
 app.use(express.json());
 app.use(express.urlencoded());
 
+async function findArtistIdByName(searched_name){
+   var artist=await Artists.findOne({where:{name:searched_name}});
+    if(artist){
+    return artist.id;
+    }
+   else{
+       return null;
+     }
+}
+
+app.post('/geoTracks/:country',(request,response)=>{
+    let url='https://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country='+request.params.country+'&api_key=3516736128cc24d429fa4a04d2ef2d7b&format=json&limit=20'
+   GeoTracks.findAll().then(function(track){
+        if(track){
+            for(var i=0;i<track.length;i++){
+                track[i].destroy();
+            }
+             console.log("sterse");
+        }
+       
+    })
+   axios.get(url).then(async(result) => {
+        
+        for(let i=0;i<20;i++){
+        var name=result.data.tracks.track[i].name;
+        var noOfListeners=result.data.tracks.track[i].listeners;
+        var url=result.data.tracks.track[i].url;
+        var image=result.data.tracks.track[i].image[1]['#text'];
+        var rank=result.data.tracks.track[i]['@attr'].rank;
+        var country=request.params.country;
+        let artist_name=result.data.tracks.track[i].artist.name;
+    
+        try{
+           var id_artist = await findArtistIdByName(artist_name);
+          await  GeoTracks.create({"id":i+1,
+                          "name":name,
+                          "listeners":noOfListeners,
+                          "url":url,
+                          "image":image,
+                          "rank":rank,
+                          "country":country,
+                          "id_artist":id_artist
+        })
+        }catch(error){
+            
+            console.log("Error message: "+error.message);
+        }
+}
+    
+    response.status(200).send('The data has been successfully inserted into the table');
+     
+    })
+    
+   
+})
+app.get("/geoTrackList",async function(request,response){
+    try{
+   let geo_tracks= await GeoTracks.findAll();
+   response.status(200).json(geo_tracks);
+    }catch(error){
+        response.status(500).send(error.message);
+    }
+})
+
 
 app.post('/account',function(request,response){
     Accounts.create(request.body).then(function(account){
@@ -138,4 +197,46 @@ app.post('/account',function(request,response){
         response.status(401).send(error.message);
     })
 })
+
+app.post('/preferences',function(request,response){
+    Preferences.create(request.body).then((p)=>{
+        response.status(201).json(p)
+    }).catch(error=>{
+        response.status(400).send(error.message)
+    })
+})
+
+app.get('/preferenceList',async function(request,response){
+    try{
+   let preferences= await Preferences.findAll()
+        response.status(200).json(preferences)
+    }catch(error){
+        response.status(500).send(error.message)
+    }
+})
+app.put('/updatePreference/:track_name/:id_user',async function(request, response) {
+    try{
+        let preference = await Preferences.findOne(
+            {
+                where:{track_name:request.params.track_name,
+                id_user:request.params.id_user}
+                
+            })
+        if(preference){
+            
+            await preference.update(request.body)
+            response.status(200).send("The preference has been updated.")
+            
+        }else{
+            
+            response.status(404).send("Preference not found.")
+        }
+        
+    }catch(error){
+        response.status(500).send(error.message)
+    }
+    
+})
+
+
 app.listen(8080);
