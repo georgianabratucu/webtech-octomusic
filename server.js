dfa7d98ff40a7151833485943684f1030a86d853
Bratucu Iuliana <iuliana.bratucu.97@gmail.com>
2018-12-09 16:17:32 +0000

    add put for artist

--- server.js
+++ server.js
@@ -384,6 +384,37 @@ app.delete("/deleteArtist/:name", async function(request,response){
     }
 });
 
+//modify an artist by name
+app.put('/updateArtist/:name',async function(request,response){
+    
+    try {
+        let artist=await Artists.findOne(
+            {
+                where:
+                {
+                    name:request.params.name
+                    
+                }
+                
+            });
+            
+        if(artist){
+            
+            await artist.update(request.body);
+            response.status(200).json(artist);
+        }
+        else {
+            
+            response.status(404).send("The artist was not found!");
+        }
+        
+    } catch(error){
+        
+        response.status(500).send(error.message);
+    }
+  
+});
+
 //function to bind Artists and geoTracks/genreTracks tables by id artist
 async function findArtistIdByName(searched_name){
    var artist=await Artists.findOne({where:{name:searched_name}});
