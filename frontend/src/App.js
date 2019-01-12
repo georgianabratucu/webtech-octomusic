3e59ee5fa02c4cd249fff70e795c8e3ae3a83fce
Bratucu Georgiana <bratucu_georgiana@yahoo.com>
2019-01-12 14:06:23 +0000

    react

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+       proicet
+      </div>
+    );
+  }
+}
+
+export default App;
